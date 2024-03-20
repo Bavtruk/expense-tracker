ae07e491ded9d04a5d1ef325d4db6adc68309c98
Your Name <you@example.com>
2024-03-20 10:17:53 +0200

    tracker

--- main.py
+++ main.py
@@ -10,14 +10,14 @@
 # Uzrakstīt programmu, kas ļauj
 # - ievadīt izdevumus: nosaukumu, summu un kategoriju
 # - atspoguļot uz ekrāna visus izdevumus
-# - iespēja atlasīt 10 lielākus izdevumus
-# - iespēja atlasīt 10 mazākus izdevumus
-# - iespēja apskatīt vidējo izdevumu summu
+# iespēja atlasīt 10 lielākus izdevumus  
+ #- iespēja atlasīt 10 mazākus izdevumus
+ #- iespēja apskatīt vidējo izdevumu summu 
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
 
 expenses = []
-
+import json
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
 pass
@@ -25,6 +25,17 @@ pass
 while True:
     command = input("\nChoose command:")
     if command == "1":
+        print("Uz ko patereji")
+        nosaukums = input("nosaukums")
+        summa = input("summa")
+        kategorija = input("kategorija")
+        izdevumi = {"nosaukums" : nosaukums, "summa" : summa,  "kategorija" : kategorija}
+        expenses.append(izdevumi)
+        pass
+    if command == "2":
+        expenses.append(izdevumi)
+        with open("sample.json", "w") as outfile:
+            json.dump(expenses, outfile)
         pass
     if command == "e":
         print("Exiting...")
